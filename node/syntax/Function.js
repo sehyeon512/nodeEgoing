a5f7f6574b4ae90afe54465400b4b988c7a498be
김세현 <90504012+sehyeon512@users.noreply.github.com>
2021-10-10 16:33:27 +0900

    learn node.js 211010

---
+++ node/syntax/Function.js
@@ -0,0 +1,10 @@
+f123();
+console.log('A1');
+f123();
+console.log('B2');
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
---
+++ node/syntax/Function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1 Math.round 반올림
+
+function sum(first, second){ // parameter  매개변수
+  return first+second;
+  // return 어떤 값을 출력한다 and 함수를 종료시킨다
+}
+
+console.log(sum(2,4)); // argument 인자
